a6fed42f956c44d5adc4418342084ed42665dada
Satit Porntepanon <st_satitza@hotmail.com>
2025-01-14 18:00:56 +0700

    add configuration

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"go-minio-file-upload/configuration"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Println("Golang minio file upload")
+
+	appConfig, err := configuration.GetConfig()
+	if err != nil {
+		log.Fatalf("error read configuration. %s\n", err.Error())
+	}
+
+	fmt.Printf("port %d\n", appConfig.Port)
 	os.Exit(0)
 }
